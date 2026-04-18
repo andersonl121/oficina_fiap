272f2f9708a5bddbbfaf19fdb332b72ff1b2ab1b
Guilherme Menoci Pessoal <guimenoci@gmail.com>
2026-04-18 19:39:52 -0300

    remove comments

--- src/main/java/br/com/fiap/soat15/tc_oficina/infrastructure/repository/ClienteRepository.java
+++ src/main/java/br/com/fiap/soat15/tc_oficina/infrastructure/repository/ClienteRepository.java
@@ -1,7 +1,7 @@
-//package br.com.fiap.soat15.tc_oficina.infrastructure.repository;
-//
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface ClienteRepository {
-//}
+package br.com.fiap.soat15.tc_oficina.infrastructure.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository {
+}
