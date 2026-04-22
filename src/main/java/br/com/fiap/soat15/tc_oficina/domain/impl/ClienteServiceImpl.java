7865c7b4df248c1810425c6b1d3bbe697446cb8d
Guilherme Menoci Pessoal <guimenoci@gmail.com>
2026-04-22 20:28:29 -0300

    remove comments

--- src/main/java/br/com/fiap/soat15/tc_oficina/domain/impl/ClienteServiceImpl.java
+++ src/main/java/br/com/fiap/soat15/tc_oficina/domain/impl/ClienteServiceImpl.java
@@ -2,7 +2,7 @@ package br.com.fiap.soat15.tc_oficina.domain.impl;
 
 import br.com.fiap.soat15.tc_oficina.domain.ClienteService;
 import br.com.fiap.soat15.tc_oficina.domain.model.ClienteDto;
-//import br.com.fiap.soat15.tc_oficina.infrastructure.repository.ClienteRepository;
+import br.com.fiap.soat15.tc_oficina.infrastructure.repository.ClienteRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,11 +10,11 @@ import java.util.List;
 @Service
 public class ClienteServiceImpl implements ClienteService {
 
-//    private ClienteRepository clienteRepository;
-//
-//    public ClienteServiceImpl(ClienteRepository clienteRepository) {
-//        this.clienteRepository = clienteRepository;
-//    }
+    private ClienteRepository clienteRepository;
+
+    public ClienteServiceImpl(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
 
     @Override
     public List<ClienteDto> getCliente() {
