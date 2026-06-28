81642af9cf93f98a70083b36e85e73c41761fd5a
Anderson Lima Machado <anderson.lm30@gmail.com>
2026-06-27 21:48:47 -0300

    Ajuste testes de integracao

--- src/test/java/br/com/fiap/soat15/tc_oficina/adapter/in/web/ServicoControllerIntegrationTest.java
+++ src/test/java/br/com/fiap/soat15/tc_oficina/adapter/in/web/ServicoControllerIntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -21,6 +22,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@Sql(
+        scripts = "/cleanup.sql",
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+)
 class ServicoControllerIntegrationTest {
 
     private MockMvc mockMvc;
@@ -36,7 +41,6 @@ class ServicoControllerIntegrationTest {
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-        servicoRepository.deleteAll();
     }
 
     @Test
